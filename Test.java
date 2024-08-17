5d91807097c2c51d0ac4bea2d0048f472e1e8e71
Asim2004(Asim Kazi) <150136594+asim-kazi@users.noreply.github.com>
2024-08-17 16:07:15 +0530

    Add files via upload
    This code is parameter based program ..
    Q. Program to create interface resizable with methods resize width and height that allows objects to resize class reactangle that implements the resizable implements..

--- Test.java
+++ Test.java
@@ -1,61 +1,49 @@
-// Write a program to demonstrate single inheritance by creating a class Person
-// and deriving classes Student & Employee from it. Define class person having data
-// members name, birthdate, height, weight, address and calculateAge method.
-// Derive class Student with members as roll no, marks, calculateAvg().Also derive
-// class Employee with members as empId, salary, calculateTax().
-
-// import java.time.LocalDate;
-import java.util.Scanner;
-
-
-class Person 
+interface Resizable 
 {
-    String name;
-    int day , month ,year;
-    //LocalDate birthdate;
-    float height;
-    float weight;
-    String address;
-    
-    public void Input()
+    void resizeWidth(int width);
+    void resizeHeight(int height);
+}
+
+class Rectangle implements Resizable 
+{
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Name : ");
-        name = sc.next();
-
-        System.out.println("Enter Birthdate : ");
-        System.out.println("Day : ");
-        day = sc.nextInt();
-        System.out.println("Month : ");
-        month =  sc.nextInt();
-        System.out.println("Year : ");
-        year =  sc.nextInt();
-
-        System.out.println("Enter Your Height : ");
-        height = sc.nextFloat();
-
-        System.out.println("Enter Your Weight : ");
-        weight = sc.nextFloat();
-
-        sc.nextLine();
-        System.out.println("Enter Your Address : ");
-        address = sc.nextLine();
+        this.width = width;
+        this.height = height;
     }
 
-    void calculateAge()
+    public void resizeWidth(int width) 
     {
-        Scanner sc = new Scanner(System.in);
-        int current;
-        System.out.println("Enter Current Year : ");
-        current = sc.nextInt();
-        int diff = (current-year);
-        System.out.println("Age is : "+diff);
+        this.width = width;
     }
+
+    public void resizeHeight(int height) 
+    {
+        this.height = height;
+    }
+
+    public void print1()
+    {
+        System.out.println("Before : Width : "+width+" "+"Height : "+height+"");
+    } 
+
+    public void print2()
+    {
+        System.out.println("After : Width : "+width+" "+"Height : "+height+"");
+    } 
+}
+
+class Test
+{
     public static void main(String[] args)
     {
-        Person p1 = new Person();
-        p1.Input();
-        p1.calculateAge();
+        Rectangle rectangle = new Rectangle(100, 150);
+        rectangle.print1();
+        rectangle.resizeWidth(25);
+        rectangle.resizeHeight(55);
+        rectangle.print2();
     }
-
 }
