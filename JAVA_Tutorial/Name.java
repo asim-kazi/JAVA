1790d46e4d7b81e948293caae76f132b119e04d2
Asim Kazi <150136594+asim-kazi@users.noreply.github.com>
2024-10-25 22:42:32 +0530

    Add files via upload

---
+++ JAVA_Tutorial/Name.java
@@ -0,0 +1,13 @@
+//Print the name in between the sentence.... 
+
+import java.util.Scanner;
+
+public class Name 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name : ");
+        String name = sc.next();
+        System.out.println("Hello "+name+" Have a Good Day !!!!");
+    }    
+}
---
+++ JAVA_Tutorial/Sum.java
@@ -0,0 +1,9 @@
+// Find the sum of three numbers...
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 6,b = 9,c = 5;
+        int sum = a+b+c;
+        System.out.println("Sum of Three Number Is : "+sum);
+    }
+}
